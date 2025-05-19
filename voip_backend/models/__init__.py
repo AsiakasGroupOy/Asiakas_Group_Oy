4328d9049a1d250eaf623a30327bfc50ea88174f
IrumShehryar <irum.shehryar@gmail.com>
2025-05-20 01:09:28 +0300

    Separated database,table creating files and moved th logic for automatically loading the statuses inside table creation.

--- voip_backend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-
-# models/__init__.py
-# This file makes 'models' a Python package.
-
-# (leave it empty or just put a comment like this)
--- voip_backend/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .contact_schemas import ContactSchema
-from .organization_schemas import OrganizationSchema
-from .status_schemas import StatusSchema
-from .calling_list_schemas import CallingListSchema
-from .call_log_schemas import CallLogSchema
