f324cdabe7b7d87da1f00485cbb537a4b8bc8b01
lTanjal <tatiana.lyubavskaya@gmail.com>
2026-05-06 17:46:15 +0300

    feat: implement language switcher with i18n support (EN/FI)

--- asiakas_frontend/vite.config.js
+++ asiakas_frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: {
+    /*hmr: {
       clientPort: 443,
       port: 5173,
-    },
+    },*/
     host: true,
     allowedHosts: ["bradytelic-derek-gloopily.ngrok-free.dev", "localhost"],
     proxy: {
